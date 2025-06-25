5309e27379ed8010f9c815f70a12c3162f097dc3
Shiroroi <nathancuanga2005@gmail.com>
2025-06-25 22:46:06 +0800

    Mechanic Change
    Press E to collect
    
    Collectibles blink for 3 seconds before vanishing

--- Assets/Collectible.cs
+++ Assets/Collectible.cs
@@ -2,22 +2,76 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    private bool canBeCollected = false;
+    private bool playerInRange = false;
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Ground"))
+        if (collision.collider.CompareTag("Ground") && !canBeCollected)
         {
-            Destroy(gameObject);
-        }
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.linearVelocity = Vector2.zero;
+                rb.angularVelocity = 0f;
+                rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
 
-        if (collision.collider.CompareTag("Player"))
+            StartCoroutine(BlinkAndTimeout());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        if (canBeCollected && playerInRange && Input.GetKeyDown(KeyCode.E))
         {
             ScoreManager scoreManager = FindFirstObjectByType<ScoreManager>();
             if (scoreManager != null)
             {
-                scoreManager.AddScore(1); // +1 score
+                scoreManager.AddScore(1);
             }
 
             Destroy(gameObject);
         }
+    }
+
+    System.Collections.IEnumerator BlinkAndTimeout()
+    {
+        canBeCollected = true;
+
+        float blinkTime = 3f;
+        float blinkRate = 0.2f;
+        float elapsed = 0f;
+
+        while (elapsed < blinkTime)
+        {
+            sr.enabled = !sr.enabled;
+            yield return new WaitForSeconds(blinkRate);
+            elapsed += blinkRate;
+        }
+
+        Destroy(gameObject);
     }
 }
