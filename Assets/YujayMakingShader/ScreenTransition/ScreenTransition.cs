0d8225c3f863c9c92f0509e73afe55523eeb852b
yinquest11 <yinquest11@gmail.com>
2025-08-12 03:52:49 +0800

    crafting canvas screen transiton

--- Assets/YujayMakingShader/ScreenTransition/ScreenTransition.cs
+++ Assets/YujayMakingShader/ScreenTransition/ScreenTransition.cs
@@ -10,16 +10,24 @@ public class ScreenTransition : MonoBehaviour
     [SerializeField] private float transitionTime = 1f;
 
     [SerializeField] private string propertyName = "_Progress";
-    
+
+    private float delayTime = 1f;
     public UnityEvent OnTransitionDone;
+    
+    
 
     private void Start()
     {
+        
         StartCoroutine(TransitionCoroutine());
     }
-
+    
+    
     private IEnumerator TransitionCoroutine()
     {
+        screenTransitionMaterial.SetFloat(propertyName, 0f);
+        yield return new WaitForSeconds(delayTime);
+        
         float currentTime = 0f;
         while (currentTime < transitionTime)
         {
